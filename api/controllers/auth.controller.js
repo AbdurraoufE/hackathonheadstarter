3ea7888595d88c27999c994cb2a83bf1ec988ee1
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-27 13:41:43 -0600

    added endpoints for routering with controllers

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req,res) =>{
+    //database for registering
+}
+
+export const login = (req,res) =>{
+    //database for login
+}
+
+export const logout = (req,res) =>{
+    //database for logout
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,15 +1,10 @@
 import express from 'express';
+import { register } from '../controllers/auth.controller.js';
 
 const auth = express.Router();
 
-router.post("/register", (req,res) =>{
-    console.log("Register");
-})
+router.post("/register", register);
 
-router.post("/lgoin", (req,res) =>{
-    console.log("Register");
-})
+router.post("/lgoin", login);
 
-router.post("/logout", (req,res) =>{
-    console.log("Register");
-})
+router.post("/logout", logout);
