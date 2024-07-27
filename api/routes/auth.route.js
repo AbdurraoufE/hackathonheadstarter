39819ab75319b1062e7b2b258232461c3bbe0bd5
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-27 13:37:55 -0600

    added post routers to auth

---
+++ api/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const auth = express.Router();
+
+router.post("/register", (req,res) =>{
+    console.log("Register");
+})
+
+router.post("/lgoin", (req,res) =>{
+    console.log("Register");
+})
+
+router.post("/logout", (req,res) =>{
+    console.log("Register");
+})
