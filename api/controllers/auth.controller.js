a29a73c3f2fce2bdc112fc76146349ddc92a173f
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-27 15:39:41 -0600

    added new user to db

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,23 @@
 import bcrypt from "bcrypt";
+import prisma from "./lib/prisma.js";
 
 export const register = async (req,res) =>{
     const {username, email, password} = req.body;
-    //database for registering
     
     // hashed password
-    const hasedPass = await bcrypt.hash(password, 10);
-    console.log(hasedPass);
-    // new user & save to database
+    const hashedPass = await bcrypt.hash(password, 10);
+    console.log(hashedPass);
 
+    // new user & save to database
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password: hashedPass
+        },
+    });
+    
+    console.log(newUser);
 }
 
 export const login = (req,res) =>{
