b4211f25d12084f22a8ee6b21990e1b227bfe265
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-27 19:43:40 -0600

    mongodb connected

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
+// api/controllers/auth.controller.js
 import bcrypt from "bcrypt";
-import prisma from "./lib/prisma.js";
+import prisma from "../lib/prisma.js";  // Adjusted to reflect the correct path
 
-export const register = async (req,res) =>{
-    const {username, email, password} = req.body;
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
     
     // hashed password
     const hashedPass = await bcrypt.hash(password, 10);
@@ -10,20 +11,21 @@ export const register = async (req,res) =>{
 
     // new user & save to database
     const newUser = await prisma.user.create({
-        data:{
+        data: {
             username,
             email,
-            password: hashedPass
+            password: hashedPass,
         },
     });
     
     console.log(newUser);
-}
+    res.status(201).json(newUser);  // Respond with the new user data
+};
 
-export const login = (req,res) =>{
-    //database for login
-}
+export const login = (req, res) => {
+    // database for login
+};
 
-export const logout = (req,res) =>{
-    //database for logout
-}
+export const logout = (req, res) => {
+    // database for logout
+};
